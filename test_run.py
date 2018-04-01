55016d41f03bc409dc65a93bf074afd005ddf26c
kimtree <iam@kimtree.net>
2018-04-01 18:55:18 +0900

    Update testing file

--- test_run.py
+++ test_run.py
@@ -4,8 +4,38 @@ from ktp.parser import KorailTimeTableParser
 parser = KorailTimeTableParser('')
 parser.parse()
 
-trains = parser.get_trains()
-for train in trains:
-    pprint.pprint([train.get_idx(), train.get_name(), train.get_remarks()])
-    pprint.pprint(train.get_stops())
+import csv
+with open('trains.csv', 'w', newline='') as csvfile:
+    trains = parser.get_trains()
+
+    for train in trains:
+        stops = train.get_stops()
+
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(
+            [train.get_idx(),
+             train.get_name(),
+             stops[0].station_idx,
+             stops[-1].station_idx,
+             train.get_remarks()])
+
+    print('DONE')
+
+with open('stops.csv', 'w', newline='') as csvfile:
+    trains = parser.get_trains()
+    i = 1
+    for train in trains:
+        stops = train.get_stops()
+
+        for stop in stops:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(
+                [i,
+                 stop.line_name,
+                 train.get_idx(),
+                 stop.station_idx,
+                 stop.time])
+            i += 1
+
+    print('DONE')
 
